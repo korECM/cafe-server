3cf268d95a10ef0886a5e273462ee79f35a30b09
korECM <eatingcookieman@gmail.com>
2022-03-27 16:22:52 +0900

    feat: Member에 필수 값들 추가된거 추가

--- src/main/kotlin/study/cafe/entity/Member.kt
+++ src/main/kotlin/study/cafe/entity/Member.kt
@@ -1,14 +1,23 @@
 package study.cafe.entity
 
 import study.cafe.entity.common.BaseClass
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
+import java.time.LocalDate
+import javax.persistence.*
+import javax.persistence.EnumType.STRING
 import javax.persistence.GenerationType.IDENTITY
-import javax.persistence.Id
 
 @Entity
-class Member(val name: String) : BaseClass() {
+class Member(
+    @Column(nullable = false)
+    val nickName: String,
+
+    @Column(nullable = false)
+    var birthDay: LocalDate,
+
+    @Enumerated(STRING)
+    @Column(nullable = false)
+    var gender: Gender
+) : BaseClass() {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "user_id", nullable = false)
