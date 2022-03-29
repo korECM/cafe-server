3b78cb890d562c0879260b34cbdb55a1151022d6
korECM <eatingcookieman@gmail.com>
2022-03-30 00:46:15 +0900

    feat: Gender에 주민번호 뒷자리로부터 성별로 변환하는 메소드 추가

--- src/main/kotlin/study/cafe/entity/Gender.kt
+++ src/main/kotlin/study/cafe/entity/Gender.kt
@@ -1,5 +1,13 @@
 package study.cafe.entity
 
 enum class Gender {
-    MALE, FEMALE
+    MALE, FEMALE;
+
+    companion object
+}
+
+fun Gender.Companion.fromResidentRegistrationNumber(firstDigit: Number) = when (firstDigit) {
+    1, 3, 5, 7, 9 -> Gender.MALE
+    0, 2, 4, 6, 8 -> Gender.FEMALE
+    else -> throw IllegalArgumentException("$firstDigit 은 유효하지 않은 주민번호 뒷자리 첫 번째 숫자입니다")
 }
