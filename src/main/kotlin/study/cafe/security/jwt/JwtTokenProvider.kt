914caa19b2976e3f5aeee2c3fc2d7e17d5462336
korECM <eatingcookieman@gmail.com>
2022-04-02 22:31:22 +0900

    feat: JWTTokenProvider 작성

---
+++ src/main/kotlin/study/cafe/security/jwt/JwtTokenProvider.kt
@@ -0,0 +1,60 @@
+package study.cafe.security.jwt
+
+import io.jsonwebtoken.*
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import study.cafe.util.logger
+import study.cafe.util.plus
+import java.util.*
+
+@Component
+class JwtTokenProvider(
+    @Value("\${jwt.secret}")
+    private val secretToken: String,
+    @Value("\${jwt.token-validity-in-secnods}")
+    private val tokenValidityInMs: Long
+) {
+    private val log = logger()
+
+    private val key: String = Base64.getEncoder().encodeToString(secretToken.toByteArray(Charsets.UTF_8))
+
+    fun createToken(userPk: Long, at: Date): String {
+        val claims = Jwts.claims().setSubject(userPk.toString())
+        return Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(at)
+            .setExpiration(at + tokenValidityInMs)
+            .signWith(SignatureAlgorithm.HS512, key)
+            .compact()
+    }
+
+    fun getUserPk(token: String): Long {
+        val claims = parseClaims(token)
+        requireNotNull(claims) { "올바른 JWT 토큰이 아니라 값을 추출할 수 없습니다" }
+        try {
+            return claims.subject.toLong()
+        } catch (e: java.lang.NumberFormatException) {
+            throw IllegalArgumentException("올바른 JWT 토큰이 아니라 값을 추출할 수 없습니다")
+        }
+    }
+
+    fun validateToken(token: String): Boolean {
+        try {
+            parseClaims(token)
+            return true
+        } catch (e: SecurityException) {
+            log.info("잘못된 JWT 서명")
+        } catch (e: MalformedJwtException) {
+            log.info("잘못된 JWT 서명")
+        } catch (e: ExpiredJwtException) {
+            log.info("만료된 JWT 토큰")
+        } catch (e: UnsupportedJwtException) {
+            log.info("지원되지 않는 JWT 토큰")
+        } catch (e: IllegalArgumentException) {
+            log.info("잘못된 JWT 토큰")
+        }
+        return false
+    }
+
+    private fun parseClaims(token: String): Claims? = Jwts.parser().setSigningKey(key).parseClaimsJwt(token).body
+}
