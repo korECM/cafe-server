c8348b0764a65570fb841a8bca71904dd4dbe85d
korECM <eatingcookieman@gmail.com>
2022-04-02 18:28:38 +0900

    feat: ApiResponse에 Swagger doc 추가

--- src/main/kotlin/study/cafe/api/dto/ApiResponse.kt
+++ src/main/kotlin/study/cafe/api/dto/ApiResponse.kt
@@ -1,6 +1,9 @@
 package study.cafe.api.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class ApiResponse<T>(
+    @Schema(title = "에러 메시지")
     val message: String? = "",
     val body: T? = null
 ) {
