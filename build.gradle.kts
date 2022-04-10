0e8c6f6f1b3f5d311ac54de6674bfb25a78b07e7
korECM <eatingcookieman@gmail.com>
2022-04-11 00:39:57 +0900

    deps: jbcrypt 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,11 +31,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     implementation("io.jsonwebtoken:jjwt:0.9.1")
+    implementation("org.mindrot:jbcrypt:0.4")
 
     implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.8.0")
 
