e1e5261e85586a772f19f6c5db98d084bff14fc7
korECM <eatingcookieman@gmail.com>
2022-04-16 23:39:07 +0900

    feat: Health Check 추가

---
+++ src/main/kotlin/study/cafe/api/HealthController.kt
@@ -0,0 +1,17 @@
+package study.cafe.api
+
+import io.swagger.v3.oas.annotations.Operation
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.ok
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+
+    @Operation(summary = "헬스 체크", description = "헬스 체크용 엔드포인트")
+    @GetMapping("/health")
+    fun health(): ResponseEntity<String> {
+        return ok("Hello")
+    }
+}
