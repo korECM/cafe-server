f4c8de9e0e4fc1915d9b1cffa2a0887839ad6628
ECMs <youncookiexpire@kakao.com>
2022-05-20 20:29:08 +0900

    feat: 좌표로부터 편하게 Point를 생성할 수 있는 유틸 함수 추가

---
+++ src/main/kotlin/study/cafe/util/PointUtils.kt
@@ -0,0 +1,9 @@
+package study.cafe.util
+
+import org.locationtech.jts.geom.Point
+import org.locationtech.jts.io.WKTReader
+
+fun createPoint(longitude: Double, latitude: Double): Point {
+    val pointWKT = String.format("POINT(%s %s)", longitude, latitude)
+    return WKTReader().read(pointWKT) as Point
+}
