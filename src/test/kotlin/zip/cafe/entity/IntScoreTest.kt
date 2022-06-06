e3a831373ee9bdf0c192de5cd855e47ee985b058
ECMs <youncookiexpire@kakao.com>
2022-06-06 18:24:31 +0900

    refac: IntScore 테스트 withData 형식으로 변경

--- src/test/kotlin/zip/cafe/entity/IntScoreTest.kt
+++ src/test/kotlin/zip/cafe/entity/IntScoreTest.kt
@@ -1,25 +1,22 @@
 package zip.cafe.entity
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.assertThrows
 
 class IntScoreTest : FreeSpec({
+    val validScoreList = listOf(1, 2, 3, 4, 5)
+
     "1~5 사이가 아니면 IllegalArgumentException을 던진다" {
         assertThrows<IllegalArgumentException> { IntScore(-3) }
         assertThrows<IllegalArgumentException> { IntScore(0) }
         assertThrows<IllegalArgumentException> { IntScore(6) }
     }
-    "1부터 5사이면 그 숫자를 가지는 IntScore를 생성한다" {
-        // given
-        val score1 = IntScore(1)
-        val score2 = IntScore(3)
-        val score3 = IntScore(5)
-        // when
-        // then
-        score1.score shouldBe 1
-        score2.score shouldBe 3
-        score3.score shouldBe 5
+    "1부터 5사이면 그 숫자를 가지는 IntScore를 생성한다" - {
+        withData(validScoreList) { score ->
+            IntScore(score).score shouldBe score
+        }
     }
 
     "확장 함수로 IntScore를 생성할 수 있다" {
