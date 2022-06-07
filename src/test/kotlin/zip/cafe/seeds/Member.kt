cd19f9f2a9d45df7ed7a1512d52793d0e0326ced
ECMs <youncookiexpire@kakao.com>
2022-06-07 22:29:21 +0900

    feat: MemberSeed 생성할 때 id 넣을 수 있도록 추가

--- src/test/kotlin/zip/cafe/seeds/Member.kt
+++ src/test/kotlin/zip/cafe/seeds/Member.kt
@@ -3,16 +3,21 @@ package zip.cafe.seeds
 import zip.cafe.entity.member.Gender
 import zip.cafe.entity.member.Member
 import zip.cafe.utils.faker
+import zip.cafe.utils.setEntityId
 import java.time.LocalDate
 
 fun createMember(
+    id: Long = faker.random.nextLong(),
     name: String = faker.name.name(),
     nickname: String = faker.name.name(),
     birthDay: LocalDate = faker.person.birthDate(faker.random.nextInt(10, 50).toLong()),
     gender: Gender = faker.random.nextEnum(Gender::class.java)
-): Member = Member(
-    name = name,
-    nickname = nickname,
-    birthDay = birthDay,
-    gender = gender
+): Member = setEntityId(
+    id,
+    Member(
+        name = name,
+        nickname = nickname,
+        birthDay = birthDay,
+        gender = gender
+    )
 )
