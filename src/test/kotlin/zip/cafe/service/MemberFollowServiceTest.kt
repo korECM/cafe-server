7c25e6b6b61e5bfa8cf9a8d60e92560e5cb985cd
ECMs <youncookiexpire@kakao.com>
2022-06-07 22:57:03 +0900

    feat: MemberFollowService 언팔로우 테스트 추가

--- src/test/kotlin/zip/cafe/service/MemberFollowServiceTest.kt
+++ src/test/kotlin/zip/cafe/service/MemberFollowServiceTest.kt
@@ -2,6 +2,7 @@ package zip.cafe.service
 
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.collections.shouldHaveSize
 import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
@@ -50,6 +51,48 @@ class MemberFollowServiceTest : FreeSpec({
                 // then
                 fromMember.followees shouldContainExactly listOf(toMember)
                 toMember.followers shouldContainExactly listOf(fromMember)
+            }
+        }
+
+        "언팔로우" - {
+            "from이 to를 팔로우한 적이 있었다면" - {
+                "성공적으로 언팔로우한다" {
+                    // given
+                    val fromMember = createMember(id = 3L)
+                    val toMember = createMember(id = 5L)
+                    val anotherMember = createMember(id = 7L)
+                    // mock
+                    every { memberRepository.findOneById(fromMember.id) } returns fromMember
+                    every { memberRepository.findOneById(toMember.id) } returns toMember
+                    every { memberRepository.findOneById(anotherMember.id) } returns anotherMember
+                    // when
+                    memberFollowService.follow(fromMember.id, toMember.id)
+                    memberFollowService.follow(fromMember.id, anotherMember.id)
+                    memberFollowService.unfollow(fromMember.id, toMember.id)
+                    // then
+                    fromMember.followees shouldContainExactly listOf(anotherMember)
+                    toMember.followers shouldHaveSize 0
+                    anotherMember.followers shouldContainExactly listOf(fromMember)
+                }
+            }
+            "from이 to를 팔로우한 적이 없었다면" - {
+                "기존 팔로우, 팔로워 목록은 유지되고 아무런 일도 일어나지 않는다" {
+                    // given
+                    val fromMember = createMember(id = 3L)
+                    val toMember = createMember(id = 5L)
+                    val anotherMember = createMember(id = 7L)
+                    // mock
+                    every { memberRepository.findOneById(fromMember.id) } returns fromMember
+                    every { memberRepository.findOneById(toMember.id) } returns toMember
+                    every { memberRepository.findOneById(anotherMember.id) } returns anotherMember
+                    // when
+                    memberFollowService.follow(fromMember.id, toMember.id)
+                    memberFollowService.unfollow(fromMember.id, anotherMember.id)
+                    // then
+                    fromMember.followees shouldContainExactly listOf(toMember)
+                    toMember.followers shouldContainExactly listOf(fromMember)
+                    anotherMember.followers shouldHaveSize 0
+                }
             }
         }
     }
