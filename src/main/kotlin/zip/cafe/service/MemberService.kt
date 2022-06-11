b8a00e24e721e2563601fabdff832959e61dd0d6
ECMs <youncookiexpire@kakao.com>
2022-06-11 18:16:55 +0900

    fix: IllegalArgumentException이 아니라 NoSuchElement 예외를 던지도록 수정

--- src/main/kotlin/zip/cafe/service/MemberService.kt
+++ src/main/kotlin/zip/cafe/service/MemberService.kt
@@ -1,19 +1,14 @@
 package zip.cafe.service
 
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
-import zip.cafe.entity.member.Member
 import zip.cafe.repository.MemberRepository
+import zip.cafe.repository.findOneById
 
 @Service
 class MemberService(
     private val memberRepository: MemberRepository
 ) {
-    fun checkNicknameDuplication(nickname: String): Boolean {
-        return memberRepository.existsByNicknameIs(nickname)
-    }
+    fun checkNicknameDuplication(nickname: String) = memberRepository.existsByNicknameIs(nickname)
 
-    fun findMemberById(userId: Long): Member {
-        return memberRepository.findByIdOrNull(userId) ?: throw IllegalArgumentException("존재하지 않는 유저입니다")
-    }
+    fun findMemberById(memberId: Long) = memberRepository.findOneById(memberId)
 }
