e6e4e3b00b991424fe5159ba2e95b3683823d734
ECMs <youncookiexpire@kakao.com>
2022-06-12 16:22:55 +0900

    feat: CafeService 테스트 추가

---
+++ src/test/kotlin/zip/cafe/service/CafeServiceTest.kt
@@ -0,0 +1,41 @@
+package zip.cafe.service
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.mockk
+import org.springframework.data.repository.findByIdOrNull
+import zip.cafe.repository.CafeRepository
+import zip.cafe.seeds.createCafe
+
+class CafeServiceTest : FreeSpec({
+
+    val cafeRepository = mockk<CafeRepository>(relaxed = true)
+    val cafeService = CafeService(cafeRepository = cafeRepository)
+
+    "findById" - {
+        "id로 카페를 가져온다" {
+            // given
+            val cafeId = 15L
+            val cafe = createCafe(id = cafeId)
+            // mock
+            every { cafeRepository.findByIdOrNull(cafeId) } returns cafe
+            // when
+            val findCafe = cafeService.findById(cafeId)
+            // then
+            findCafe shouldBe cafe
+        }
+        "해당 id의 카페가 없으면 NoSuchElementException 예외를 던진다" {
+            // given
+            val cafeId = 15L
+            // mock
+            every { cafeRepository.findByIdOrNull(cafeId) } returns null
+            // when
+            shouldThrow<NoSuchElementException> { cafeService.findById(cafeId) }
+        }
+    }
+
+    afterTest { clearMocks(cafeRepository) }
+})
