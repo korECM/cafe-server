fac96f74359638207188036c9052fe8bd7dffdc4
ECMs <youncookiexpire@kakao.com>
2022-06-12 16:04:44 +0900

    feat: cafe seeds 추가

--- src/main/kotlin/zip/cafe/util/PointUtils.kt
+++ src/main/kotlin/zip/cafe/util/PointUtils.kt
@@ -3,6 +3,9 @@ package zip.cafe.util
 import org.locationtech.jts.geom.Point
 import org.locationtech.jts.io.WKTReader
 
+val seoulLatitude = 37.532600
+val seoulLongitude = 127.024612
+
 fun createPoint(longitude: Double, latitude: Double): Point {
     val pointWKT = String.format("POINT(%s %s)", longitude, latitude)
     return WKTReader().read(pointWKT) as Point
---
+++ src/test/kotlin/zip/cafe/seeds/Cafe.kt
@@ -0,0 +1,23 @@
+package zip.cafe.seeds
+
+import org.locationtech.jts.geom.Point
+import zip.cafe.entity.cafe.Cafe
+import zip.cafe.util.createPoint
+import zip.cafe.util.seoulLatitude
+import zip.cafe.util.seoulLongitude
+import zip.cafe.utils.faker
+import zip.cafe.utils.setEntityId
+
+fun createCafe(
+    id: Long = faker.random.nextLong(),
+    name: String = faker.name.name(),
+    address: String = faker.address.fullAddress(),
+    location: Point = createPoint(seoulLongitude + faker.random.nextDouble() / 2, seoulLatitude + faker.random.nextDouble() / 2)
+) = setEntityId(
+    id,
+    Cafe(
+        name = name,
+        address = address,
+        location = location,
+    )
+)
