05243898b64badadc667275eac733f11fcd314b4
ECMs <youncookiexpire@kakao.com>
2022-06-15 01:21:33 +0900

    fix: Jpa 활성화 설정 분리 for web test

--- src/main/kotlin/zip/cafe/CafeApplication.kt
+++ src/main/kotlin/zip/cafe/CafeApplication.kt
@@ -2,9 +2,7 @@ package zip.cafe
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-@EnableJpaAuditing
 @SpringBootApplication
 class CafeApplication
 
