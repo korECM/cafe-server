3e59d7e2c1ab7326eeac0e0218c7ab2ab53e1e81
ECMs <youncookiexpire@kakao.com>
2022-06-18 22:03:36 +0900

    feat: review like equals and hashcode 추가

--- src/main/kotlin/zip/cafe/entity/review/ReviewLike.kt
+++ src/main/kotlin/zip/cafe/entity/review/ReviewLike.kt
@@ -1,5 +1,6 @@
 package zip.cafe.entity.review
 
+import org.hibernate.Hibernate
 import zip.cafe.entity.member.Member
 import javax.persistence.*
 import javax.persistence.GenerationType.IDENTITY
@@ -19,4 +20,13 @@ class ReviewLike(
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "review_like_id", nullable = false)
     val id: Long = 0
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as ReviewLike
+
+        return member == other.member && review == other.review
+    }
+
+    override fun hashCode() = 31 * review.hashCode() + member.hashCode()
 }
---
+++ src/test/kotlin/zip/cafe/entity/review/ReviewLikeTest.kt
@@ -0,0 +1,18 @@
+package zip.cafe.entity.review
+
+import io.kotest.core.spec.style.FreeSpec
+import nl.jqno.equalsverifier.EqualsVerifier
+import zip.cafe.entity.member.Member
+import zip.cafe.seeds.createMember
+import zip.cafe.seeds.createReview
+
+class ReviewLikeTest : FreeSpec({
+
+    "equalsAndHashCode" {
+        EqualsVerifier.forClass(ReviewLike::class.java)
+            .withOnlyTheseFields("member", "review")
+            .withPrefabValues(Member::class.java, createMember(), createMember())
+            .withPrefabValues(Review::class.java, createReview(), createReview())
+            .verify()
+    }
+})
