a58442d96e21a69ca54a570686d4be54b7913d7e
ECMs <youncookiexpire@kakao.com>
2022-06-19 21:43:36 +0900

    feat: fetch 조인 최적화

--- src/main/kotlin/zip/cafe/repository/ReviewRepository.kt
+++ src/main/kotlin/zip/cafe/repository/ReviewRepository.kt
@@ -8,6 +8,6 @@ import zip.cafe.entity.review.Review
 fun ReviewRepository.findOneById(id: Long) = this.findByIdOrNull(id) ?: throw NoSuchElementException()
 
 interface ReviewRepository : JpaRepository<Review, Long> {
-    @Query("select r from Review r where r.member.id in :authorIds")
+    @Query("select distinct r from Review r inner join fetch r.cafe inner join fetch r.member left join fetch r._images left join fetch r._cafeKeywords ck left join fetch ck.cafeKeyword left join fetch r._likes where r.member.id in :authorIds")
     fun findByAuthorIdIn(authorIds: List<Long>): List<Review>
 }
