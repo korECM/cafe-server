d97a6c39e9d37800ee9e97308ce9111bb2defa07
ECMs <youncookiexpire@kakao.com>
2022-06-26 23:43:10 +0900

    refac: ResponseEntity 제거

--- src/main/kotlin/zip/cafe/api/ReviewController.kt
+++ src/main/kotlin/zip/cafe/api/ReviewController.kt
@@ -2,8 +2,6 @@ package zip.cafe.api
 
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.HttpStatus.NO_CONTENT
-import org.springframework.http.ResponseEntity
-import org.springframework.http.ResponseEntity.status
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import zip.cafe.api.dto.ApiResponse
@@ -23,25 +21,27 @@ class ReviewController(
     private val reviewLikeService: ReviewLikeService
 ) {
 
+    @ResponseStatus(CREATED)
     @PostMapping("")
     fun register(
         @LoginUserId userId: Long,
         @Valid @RequestBody request: ReviewRegisterRequest
-    ): ResponseEntity<ApiResponse<Nothing>> {
+    ): ApiResponse<Nothing> {
         reviewService.createReview(request.cafeId, userId, request.toDto())
-        return status(CREATED).body(success(null))
+        return success(null)
     }
 
+    @ResponseStatus(CREATED)
     @PostMapping("/images")
     fun uploadImages(
         @LoginUserId userId: Long,
         @RequestPart("images") images: List<MultipartFile>,
-    ): ResponseEntity<ApiResponse<List<UploadedImageResponse>>> {
+    ): ApiResponse<List<UploadedImageResponse>> {
         val savedReviewImages = reviewService.run {
             val uploadReviewImages = uploadReviewImages(images)
             saveUploadedReviewImage(userId, uploadReviewImages)
         }
-        return status(CREATED).body(success(savedReviewImages.map { from(it) }))
+        return success(savedReviewImages.map { from(it) })
     }
 
     @ResponseStatus(CREATED)
