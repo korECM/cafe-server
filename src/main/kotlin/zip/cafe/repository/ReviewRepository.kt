ee41e6777754624f764c551df8a8727704861509
ECMs <youncookiexpire@kakao.com>
2022-06-29 00:03:16 +0900

    fix: reviewRepo @Param 빠진거 추가

--- src/main/kotlin/zip/cafe/repository/ReviewRepository.kt
+++ src/main/kotlin/zip/cafe/repository/ReviewRepository.kt
@@ -3,11 +3,12 @@ package zip.cafe.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.data.repository.query.Param
 import zip.cafe.entity.review.Review
 
 fun ReviewRepository.findOneById(id: Long) = this.findByIdOrNull(id) ?: throw NoSuchElementException()
 
 interface ReviewRepository : JpaRepository<Review, Long> {
     @Query("select distinct r from Review r inner join fetch r.cafe inner join fetch r.member left join fetch r._images left join fetch r._cafeKeywords ck left join fetch ck.cafeKeyword left join fetch r._likes where r.member.id in :authorIds")
-    fun findByAuthorIdIn(authorIds: List<Long>): List<Review>
+    fun findByAuthorIdIn(@Param("authorIds") authorIds: List<Long>): List<Review>
 }
