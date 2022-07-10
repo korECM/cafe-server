11d31dc5c6e52683eb3f0e748a076d000216aa3d
ECMs <youncookiexpire@kakao.com>
2022-07-10 16:35:33 +0900

    feat: 테스트 어플리케이션 시작할 때 기본 데이터 초기화되도록 sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+SET foreign_key_checks = 0
+truncate table cafe
+truncate table cafe_keyword
+truncate table cafe_like
+truncate table local_auth
+truncate table member
+truncate table member_follow
+truncate table review
+truncate table review_cafe_keyword
+truncate table review_comment
+truncate table review_food_info
+truncate table review_image
+truncate table review_like
+truncate table visit_purpose_information
+SET foreign_key_checks = 1
