f0ba42213f2fecf4fa3a52da1cf9922031324a37
ECMs <youncookiexpire@kakao.com>
2022-08-04 21:36:38 +0900

    feat: path parameters rest docs 문서화 get뿐만 아니라 put, post, delete도 지원

--- src/test/kotlin/zip/cafe/api/utils/mockmvc/PerformExtension.kt
+++ src/test/kotlin/zip/cafe/api/utils/mockmvc/PerformExtension.kt
@@ -1,11 +1,24 @@
 package zip.cafe.api.utils.mockmvc
 
 import org.springframework.restdocs.generate.RestDocumentationGenerator.ATTRIBUTE_NAME_URL_TEMPLATE
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.ResultActionsDsl
-import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.*
 
 fun MockMvc.getWithPathParameter(urlTemplate: String, vararg vars: Any?): ResultActionsDsl =
     this.get(urlTemplate, *vars) {
+        requestAttr(ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate)
+    }
+
+fun MockMvc.postWithPathParameter(urlTemplate: String, vararg vars: Any?): ResultActionsDsl =
+    this.post(urlTemplate, *vars) {
+        requestAttr(ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate)
+    }
+
+fun MockMvc.putWithPathParameter(urlTemplate: String, vararg vars: Any?): ResultActionsDsl =
+    this.put(urlTemplate, *vars) {
+        requestAttr(ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate)
+    }
+
+fun MockMvc.deleteWithPathParameter(urlTemplate: String, vararg vars: Any?): ResultActionsDsl =
+    this.delete(urlTemplate, *vars) {
         requestAttr(ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate)
     }
