3820eae8962c6cdc13520f34c3c2879bf5c336e6
ECMs <youncookiexpire@kakao.com>
2022-08-04 14:28:27 +0900

    deps: equalsverifier 3.10 -> 3.10.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -97,7 +97,7 @@ dependencies {
     testImplementation("io.kotest:kotest-extensions-spring:4.4.3")
     testImplementation("io.mockk:mockk:1.12.5")
     testImplementation("com.ninja-squad:springmockk:3.1.1")
-    testImplementation("nl.jqno.equalsverifier:equalsverifier:3.10")
+    testImplementation("nl.jqno.equalsverifier:equalsverifier:3.10.1")
 
     implementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
     implementation("io.github.serpro69:kotlin-faker:1.11.0")
