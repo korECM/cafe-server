4ea672ac37a0a5a1c611c97b108b30d70a177d5b
ECMs <youncookiexpire@kakao.com>
2022-08-30 22:21:25 +0900

    feat: requestParameters 사용하도록 수정

--- src/test/kotlin/zip/cafe/api/SearchControllerTest.kt
+++ src/test/kotlin/zip/cafe/api/SearchControllerTest.kt
@@ -3,7 +3,8 @@ package zip.cafe.api
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.test.web.servlet.get
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import zip.cafe.api.utils.restdocs.*
 import zip.cafe.api.utils.spec.WebMvcTestSpec
 import zip.cafe.seeds.createCafe
@@ -23,27 +24,25 @@ class SearchControllerTest : WebMvcTestSpec() {
 
             every { searchService.searchMember(memberName) } returns listOf(createMember(name = "홍1"), createMember(name = "홍2"))
 
-            val response = mockMvc.get("/search/member") {
-                param("name", memberName)
-            }
+            val response = mockMvc.perform(get("/search/member").param("name", memberName))
 
-            response.andExpect {
-                status { isOk() }
-            }
-                .andDo {
-                    handle(
-                        document(
-                            "search-member-name",
-                            responseBody(
-                                "body" beneathPathWithSubsectionId "body",
-                                ".id" type NUMBER means "유저 id" example "3",
-                                ".name" type STRING means "유저 닉네임" example "홍길동",
-                                ".image" type STRING means "유저 이미지 URL" example "https://awsome.image.png",
-                                ".description" type STRING means "유저 설명" example "어쩌구 팬카페",
-                            )
-                        )
+            response.andExpect(
+                status().isOk
+            ).andDo(
+                document(
+                    "search-member-name",
+                    requestParameters(
+                        "name" means "멤버 이름" example memberName
+                    ),
+                    responseBody(
+                        "body" beneathPathWithSubsectionId "body",
+                        ".id" type NUMBER means "유저 id" example "3",
+                        ".name" type STRING means "유저 닉네임" example "홍길동",
+                        ".image" type STRING means "유저 이미지 URL" example "https://awsome.image.png",
+                        ".description" type STRING means "유저 설명" example "어쩌구 팬카페",
                     )
-                }
+                )
+            )
         }
 
         "카페 이름 검색" {
@@ -51,27 +50,25 @@ class SearchControllerTest : WebMvcTestSpec() {
 
             every { searchService.searchCafe(cafeName) } returns listOf(createCafe(name = "멋진 북카페"), createCafe(name = "별로인 북카페"))
 
-            val response = mockMvc.get("/search/cafe") {
-                param("name", cafeName)
-            }
+            val response = mockMvc.perform(get("/search/cafe").param("name", cafeName))
 
-            response.andExpect {
-                status { isOk() }
-            }
-                .andDo {
-                    handle(
-                        document(
-                            "search-cafe-name",
-                            responseBody(
-                                "body" beneathPathWithSubsectionId "body",
-                                ".id" type NUMBER means "카페 id" example "5",
-                                ".name" type STRING means "카페 네임" example "멋진 북카페",
-                                ".image" type STRING means "카페 이미지 URL" example "https://awsome.image.png",
-                                ".address" type STRING means "카페 주소" example "서울 강남구 봉은사로 123 5번지 3층",
-                            )
-                        )
+            response.andExpect(
+                status().isOk
+            ).andDo(
+                document(
+                    "search-cafe-name",
+                    requestParameters(
+                        "name" means "카페 이름" example cafeName
+                    ),
+                    responseBody(
+                        "body" beneathPathWithSubsectionId "body",
+                        ".id" type NUMBER means "카페 id" example "5",
+                        ".name" type STRING means "카페 네임" example "멋진 북카페",
+                        ".image" type STRING means "카페 이미지 URL" example "https://awsome.image.png",
+                        ".address" type STRING means "카페 주소" example "서울 강남구 봉은사로 123 5번지 3층",
                     )
-                }
+                )
+            )
         }
     }
 }
