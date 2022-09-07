e6b447132b8fe27d34bbac7f30fed41123156504
ECMs <youncookiexpire@kakao.com>
2022-09-07 23:25:09 +0900

    feat: 카카오 응답 추가

--- src/main/kotlin/zip/cafe/api/auth/KakaoAuthController.kt
+++ src/main/kotlin/zip/cafe/api/auth/KakaoAuthController.kt
@@ -3,9 +3,9 @@ package zip.cafe.api.auth
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.web.bind.annotation.*
 import zip.cafe.api.auth.dto.KakaoSignInRequest
+import zip.cafe.api.auth.dto.KakaoSignInResponse
 import zip.cafe.api.dto.ApiResponse
 import zip.cafe.api.dto.ApiResponse.Companion.success
-import zip.cafe.api.dto.LocalSignUpResponse
 import zip.cafe.service.auth.AuthService
 import zip.cafe.service.auth.KakaoAuthService
 
@@ -18,7 +18,7 @@ class KakaoAuthController(
 
     @ResponseStatus(CREATED)
     @PostMapping("/signIn")
-    fun signUp(@RequestBody request: KakaoSignInRequest): ApiResponse<LocalSignUpResponse> {
+    fun signUp(@RequestBody request: KakaoSignInRequest): ApiResponse<KakaoSignInResponse> {
         kakaoAuthService.getUserInfo(request.accessToken)
         return success(null)
     }
--- src/main/kotlin/zip/cafe/api/auth/dto/Kakao.kt
+++ src/main/kotlin/zip/cafe/api/auth/dto/Kakao.kt
@@ -4,3 +4,7 @@ package zip.cafe.api.auth.dto
 data class KakaoSignInRequest(
     val accessToken: String
 )
+
+data class KakaoSignInResponse(
+    val token: String
+)
