89ef799508d53ab5c98fb4bb224715ac99de41c1
ECMs <youncookiexpire@kakao.com>
2022-09-07 23:21:42 +0900

    fix: handle 빠진거 추가

--- src/test/kotlin/zip/cafe/api/utils/mockmvc/DocumentWithHandle.kt
+++ src/test/kotlin/zip/cafe/api/utils/mockmvc/DocumentWithHandle.kt
@@ -1,7 +1,6 @@
 package zip.cafe.api.utils.mockmvc
 
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
-import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
 import org.springframework.restdocs.snippet.Snippet
 import org.springframework.test.web.servlet.MockMvcResultHandlersDsl
 import zip.cafe.utils.documentRequest
@@ -10,4 +9,4 @@ import zip.cafe.utils.documentResponse
 fun MockMvcResultHandlersDsl.documentWithHandle(
     identifier: String,
     vararg snippets: Snippet
-): RestDocumentationResultHandler = MockMvcRestDocumentation.document(identifier, documentRequest, documentResponse, *snippets)
+): Unit = handle(document(identifier, documentRequest, documentResponse, *snippets))
