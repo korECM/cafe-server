85160962226e1e660ff34aa958a538bfa9c34e35
ECMs <youncookiexpire@kakao.com>
2022-09-13 21:01:58 +0900

    fix: apple auth controller mock bean 빠진거 추가

--- src/test/kotlin/zip/cafe/api/auth/AppleAuthControllerTest.kt
+++ src/test/kotlin/zip/cafe/api/auth/AppleAuthControllerTest.kt
@@ -1,5 +1,7 @@
 package zip.cafe.api.auth
 
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.post
@@ -9,12 +11,29 @@ import zip.cafe.api.utils.restdocs.STRING
 import zip.cafe.api.utils.restdocs.requestFields
 import zip.cafe.api.utils.restdocs.type
 import zip.cafe.api.utils.spec.WebMvcTestSpec
+import zip.cafe.seeds.MOCK_MVC_USER_ID
+import zip.cafe.service.auth.AppleAuthService
+import zip.cafe.service.auth.AuthService
 
 @WebMvcTest(AppleAuthController::class)
 class AppleAuthControllerTest : WebMvcTestSpec() {
+
+    @MockkBean
+    private lateinit var appleAuthService: AppleAuthService
+
+    @MockkBean
+    private lateinit var authService: AuthService
+
     init {
         "애플 로그인 / 회원가입" {
             val identityToken = "JWjHXiVIlkxciAy_fTiEft3wDaAdHvOVcV_D6wwpCinI2gAAAYMNQf1c"
+            val memberId = MOCK_MVC_USER_ID
+            val jwtToken =
+                "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1IiwiaWF0IjoxNjU4OTI4Mjg1LCJleHAiOjE2NTg5NTk4MjF9.oAc8ycJgnmM5crByz0DrvKEoH3xGceqAWPHLFtUIwTHFQopf9kbSYXsR5FML05_FUbdPIf_FGKPwo_bdIjgOyw"
+
+            every { appleAuthService.findMemberIdByAppleIdentityToken(identityToken) } returns memberId
+            every { authService.generateToken(memberId, any()) } returns jwtToken
+
 
             val response = mockMvc.post("/auth/apple/signIn") {
                 contentType = MediaType.APPLICATION_JSON
