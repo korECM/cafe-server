25fba981820fd65901574d69d57fe4509d216eba
Jongwon <youncookiexpire@kakao.com>
2022-10-08 23:05:56 +0900

    feat: withPathParameter가 dsl을 받을 수 있도록 수정

--- src/test/kotlin/zip/cafe/api/utils/mockmvc/PerformExtension.kt
+++ src/test/kotlin/zip/cafe/api/utils/mockmvc/PerformExtension.kt
@@ -3,22 +3,26 @@ package zip.cafe.api.utils.mockmvc
 import org.springframework.restdocs.generate.RestDocumentationGenerator.ATTRIBUTE_NAME_URL_TEMPLATE
 import org.springframework.test.web.servlet.*
 
-fun MockMvc.getWithPathParameter(urlTemplate: String, vararg vars: Any?): ResultActionsDsl =
+fun MockMvc.getWithPathParameter(urlTemplate: String, vararg vars: Any?, dsl: MockHttpServletRequestDsl.() -> Unit = {}): ResultActionsDsl =
     this.get(urlTemplate, *vars) {
+        dsl(this)
         requestAttr(ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate)
     }
 
-fun MockMvc.postWithPathParameter(urlTemplate: String, vararg vars: Any?): ResultActionsDsl =
+fun MockMvc.postWithPathParameter(urlTemplate: String, vararg vars: Any?, dsl: MockHttpServletRequestDsl.() -> Unit = {}): ResultActionsDsl =
     this.post(urlTemplate, *vars) {
+        dsl(this)
         requestAttr(ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate)
     }
 
-fun MockMvc.putWithPathParameter(urlTemplate: String, vararg vars: Any?): ResultActionsDsl =
+fun MockMvc.putWithPathParameter(urlTemplate: String, vararg vars: Any?, dsl: MockHttpServletRequestDsl.() -> Unit = {}): ResultActionsDsl =
     this.put(urlTemplate, *vars) {
+        dsl(this)
         requestAttr(ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate)
     }
 
-fun MockMvc.deleteWithPathParameter(urlTemplate: String, vararg vars: Any?): ResultActionsDsl =
+fun MockMvc.deleteWithPathParameter(urlTemplate: String, vararg vars: Any?, dsl: MockHttpServletRequestDsl.() -> Unit = {}): ResultActionsDsl =
     this.delete(urlTemplate, *vars) {
+        dsl(this)
         requestAttr(ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate)
     }
