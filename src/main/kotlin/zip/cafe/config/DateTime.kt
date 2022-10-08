a588103e34273dbfd476efc363e714eb74e8a5a8
Jongwon <youncookiexpire@kakao.com>
2022-10-08 22:21:45 +0900

    feat: LocalDate, LocalDateTime default format 함수 추가

--- src/main/kotlin/zip/cafe/config/DateTime.kt
+++ src/main/kotlin/zip/cafe/config/DateTime.kt
@@ -1,5 +1,12 @@
 package zip.cafe.config
 
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter.ofPattern
+
 
 const val defaultDateTimeFormat = "yyyy-MM-dd HH:mm:ss"
 const val defaultDateFormat = "yyyy-MM-dd"
+
+fun LocalDateTime.formatAsDefault(): String = this.format(ofPattern(defaultDateTimeFormat))
+fun LocalDate.formatAsDefault(): String = this.format(ofPattern(defaultDateFormat))
