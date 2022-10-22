b155f7e6a687197e5004669f9102ef4c613168f3
Jongwon <youncookiexpire@kakao.com>
2022-10-22 15:57:13 +0900

    feat: ProfileImage 클래스 추가

---
+++ src/main/kotlin/zip/cafe/entity/ProfileImage.kt
@@ -0,0 +1,35 @@
+package zip.cafe.entity
+
+import zip.cafe.entity.member.Member
+import javax.persistence.*
+import javax.persistence.FetchType.LAZY
+
+@Table(name = "profile_image")
+@Entity
+class ProfileImage(
+    bucket: String,
+    fileKey: String,
+    s3URL: String,
+    cloudFrontURL: String,
+    @ManyToOne(fetch = LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    val member: Member,
+) : Image(bucket, fileKey, s3URL, cloudFrontURL) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "profile_image_id", nullable = false)
+    val id: Long = 0
+
+    companion object {
+        fun from(bucket: String, fileKey: String, s3URL: String, cloudFrontURL: String, uploadedBy: Member): ProfileImage {
+            return ProfileImage(
+                bucket = bucket,
+                fileKey = fileKey,
+                s3URL = s3URL,
+                cloudFrontURL = cloudFrontURL,
+                member = uploadedBy,
+            )
+        }
+    }
+}
