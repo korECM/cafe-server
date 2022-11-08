1a0e7fd7d3c91cfc0a1bb7e3e9d39739a24f6dd7
Jongwon <youncookiexpire@kakao.com>
2022-11-09 00:06:34 +0900

    feat: 피드 API 테스트에 키워드 상세 정보 빠진거 추가

--- src/test/kotlin/zip/cafe/api/FeedControllerTest.kt
+++ src/test/kotlin/zip/cafe/api/FeedControllerTest.kt
@@ -42,6 +42,8 @@ class FeedControllerTest : WebMvcTestAdapter() {
             val review9 = createReview(id = 9L)
             val reviewImage9Of1 = createReviewImageWithReview()
             review9.addImage(reviewImage9Of1)
+            review9.addCafeKeyword(createCafeKeyword())
+            review9.addCafeKeyword(createCafeKeyword())
             val reviews = listOf(review1, review2, review3, review4, review5, review6, review7, review8, review9)
 
             every { feedService.getReviewFeeds(MOCK_MVC_USER_ID, 7L, limit = 3L) } returns FeedWithPagination(
@@ -98,6 +100,9 @@ class FeedControllerTest : WebMvcTestAdapter() {
                         "feeds[].review.images[].url" type STRING means "리뷰 사진 url" example "naver.com/some.png",
                         "feeds[].review.likeCount" type NUMBER means "리뷰 좋아요 개수" example "5L",
                         "feeds[].review.keyword" type ARRAY means "리뷰에 있는 키워드 목록" example "친절한 카페네요~ 커피도 맛있어요!",
+                        "feeds[].review.keyword[].id" type NUMBER means "키워드 id" example "5L",
+                        "feeds[].review.keyword[].name" type STRING means "키워드 이름" example "친절한",
+                        "feeds[].review.keyword[].emoji" type STRING means "키워드 이모지" example "✨",
                         "feeds[].review.description" type STRING means "리뷰 내용",
                         "feeds[].review.commentCount" type NUMBER means "리뷰 댓글 개수" example "3L",
                         "feeds[].review.createdAt" type DATETIME means "리뷰가 작성된 시간" formattedAs "yyyy-MM-dd HH:mm:ss" example "2022-02-12 13:52:12"
