29d4c45ddf8efbb90693b6d48b7c06d09cd79bf2
Jongwon <youncookiexpire@kakao.com>
2022-12-04 17:53:05 +0900

    fix: 인프라 바뀐거에 맞게 배포 스크립트 수정

--- test-deploy.sh
+++ test-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-CLUSTER=cafe
+CLUSTER=cafe-cluster
 SERVICE=cafe-server
 VERSION=$(git describe --tags)
-AWS_PROFILE=cafe ./gradlew :jib -Djib.to.tags=latest,"${VERSION}",test
-AWS_PROFILE=cafe aws ecs update-service --cluster ${CLUSTER} --service ${SERVICE} --force-new-deployment
+AWS_PROFILE=dns ./gradlew :jib -Djib.to.tags=latest,"${VERSION}",test
+AWS_PROFILE=dns aws ecs update-service --cluster ${CLUSTER} --service ${SERVICE} --force-new-deployment
