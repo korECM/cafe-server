5fb519cf3cb12d18d16f3adc5048cbe7219efe50
Jongwon <youncookiexpire@kakao.com>
2023-01-28 22:42:42 +0900

    fix: openingHours db 길이 늘리기

--- src/main/kotlin/zip/cafe/entity/cafe/Cafe.kt
+++ src/main/kotlin/zip/cafe/entity/cafe/Cafe.kt
@@ -21,7 +21,7 @@ class Cafe(
     @Column(name = "location", nullable = false)
     @Embedded
     val location: Point,
-    @Column(name = "opening_hours", nullable = false)
+    @Column(name = "opening_hours", nullable = false, length = 500)
     val openingHours: String,
 
     _totalScore: Double = 0.0,
