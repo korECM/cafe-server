7504211abc44b48fdc6cbddfe657cf81db27afa8
Jongwon <youncookiexpire@kakao.com>
2023-03-01 21:07:29 +0900

    fix: average transient 문제 수정

--- src/main/kotlin/zip/cafe/entity/cafe/AverageScore.kt
+++ src/main/kotlin/zip/cafe/entity/cafe/AverageScore.kt
@@ -3,21 +3,22 @@ package zip.cafe.entity.cafe
 import zip.cafe.entity.IntScore
 import javax.persistence.Column
 import javax.persistence.Embeddable
-import javax.persistence.Transient
 
 @Embeddable
 data class AverageScore(
     @Column(name = "count", nullable = false)
-    val count: Long,
+    var count: Long,
     @Column(name = "total_score", nullable = false)
     var total: Long
 ) {
 
-    @get:Transient
-    val average: Double
-        get() = if (count == 0L) 0.0 else total.toDouble() / count
+    @Column(name = "average_score", nullable = false)
+    var average: Double = if (count == 0L) 0.0 else total.toDouble() / count
+        protected set
 
     fun add(score: IntScore) {
         total += score.score
+        count += 1
+        average = total.toDouble() / count
     }
 }
