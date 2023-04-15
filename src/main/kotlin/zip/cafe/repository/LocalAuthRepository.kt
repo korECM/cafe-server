568a18a3b4107c218496c58673b067c22cbf36f6
Jongwon <youncookiexpire@kakao.com>
2023-04-15 22:24:17 +0900

    fix: 쿼리 에러나는거 수정

--- src/main/kotlin/zip/cafe/repository/LocalAuthRepository.kt
+++ src/main/kotlin/zip/cafe/repository/LocalAuthRepository.kt
@@ -10,7 +10,6 @@ interface LocalAuthRepository : JpaRepository<LocalAuth, Long> {
     @Query("select a from LocalAuth a where a.localId = :localId and a.isDeleted = false")
     fun findByLocalId(localId: String): LocalAuth?
 
-    @Query("select exists(select a from LocalAuth a where a.localId = :localId and a.isDeleted = false)")
     fun existsByLocalIdIs(localId: String): Boolean
 
     @Query("select a from LocalAuth a where a.member.id = :memberId and a.isDeleted = false")
